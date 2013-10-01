5228a34cec84a4beaf9ab9db7d99182592bb41ad
wouterkwakernaak <wouter@wouterkwakernaak.nl>
2013-10-01 12:14:41 +0200

    fixed lapd API usage

--- src/org/rascalmpl/library/LAPD.java
+++ src/org/rascalmpl/library/LAPD.java
@@ -8,6 +8,7 @@ import org.eclipse.imp.pdb.facts.IValue;
 import org.eclipse.imp.pdb.facts.IValueFactory;
 import org.rascalmpl.interpreter.TypeReifier;
 
+import lapd.neo4j.GraphDbMappingException;
 import lapd.neo4j.GraphDbValueIO;
 
 public class LAPD {
@@ -16,26 +17,20 @@ public class LAPD {
 	private IValueFactory valueFactory;
 	private TypeReifier typeReifier;
 	
-	public LAPD(IValueFactory valueFactory) {
-		try {
-			graphDbValueIO = new GraphDbValueIO(valueFactory);
-		} catch (IOException e) { }
-		this.valueFactory = valueFactory;
+	public LAPD(IValueFactory valueFactory) throws IOException {
+		graphDbValueIO = GraphDbValueIO.getInstance();
+		graphDbValueIO.init(valueFactory);
 		typeReifier = new TypeReifier(this.valueFactory);
+		this.valueFactory = valueFactory;
 	}
 
-	public void write(IString id, IValue value) {
-		try {
-			graphDbValueIO.write(id.toString(), value);
-		} catch (Exception e) {	}		
+	public void write(IString id, IValue value) throws GraphDbMappingException {
+		graphDbValueIO.write(id.toString(), value);
 	}
 
-	public IValue read(IString id, IValue reifiedType)  {
-		try {
-			org.eclipse.imp.pdb.facts.type.Type type = typeReifier.valueToType((IConstructor)reifiedType);
-			return graphDbValueIO.read(id.toString(), type);
-		} catch (Exception e) {	}		
-		return null;
+	public IValue read(IString id, IValue reifiedType) throws GraphDbMappingException  {
+		org.eclipse.imp.pdb.facts.type.Type type = typeReifier.valueToType((IConstructor)reifiedType);
+		return graphDbValueIO.read(id.toString(), type);
 	}
 
 }
